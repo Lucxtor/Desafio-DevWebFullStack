86bd1d6ec723696aa8560557034f9ff9066802af
Luis Felipe Fabiane <luisffabiane@gmail.com>
2022-08-05 16:39:17 -0300

    resolucao do Backend
    Nesse commit o intuito principal foi criar a solução que contabiliza as incidências por meio do Controller e do Use Case

--- src/modules/dividers/useCases/calculateDividers/CalculateDividersController.ts
+++ src/modules/dividers/useCases/calculateDividers/CalculateDividersController.ts
@@ -2,16 +2,26 @@ import { Response, Request } from "express";
 
 import { CalculateDividersUseCase } from "./CalculateDividersUseCase";
 
-class CreateCategoryController {
+class CalculateDividersController {
     constructor(private calculateDividersUseCase: CalculateDividersUseCase) {}
 
     handle(request: Request, response: Response): Response {
         const { n } = request.body;
 
-        this.calculateDividersUseCase.execute({ n });
+        let valor = 2;
+        let soma = 0;
+        for (valor; valor <= n; valor += 1) {
+            const dividers1 = this.calculateDividersUseCase.execute({ valor });
+            valor += 1;
+            const dividers2 = this.calculateDividersUseCase.execute({ valor });
+            valor -= 1;
+            if (dividers1 === dividers2) {
+                soma += 1;
+            }
+        }
 
-        return response.status(201).send();
+        return response.status(200).json(soma);
     }
 }
 
-export { CreateCategoryController };
+export { CalculateDividersController };
